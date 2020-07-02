c55865adc3d27b2803fed47774701087f8b6f493
Jacob Carver <jake10693@gmail.com>
2020-07-02 03:16:23 -0700

    The block now animates incrementally as intended. script. js and test.js are now matching and are both up-to-date and currently working. The next step is to add barriers on to the map.

--- script.js
+++ script.js
@@ -1,89 +1,85 @@
-        var canvas = document.querySelector('#canvas');
-        var context = canvas.getContext('2d');
+var canvas = document.querySelector('#canvas');
+var context = canvas.getContext('2d');
 
-        let xPos = 0;
-        let yPos = 0;
-        const directions = [];
-        let moveRight;
-        let moveLeft;
-        let moveUp;
-        let moveDown;
+let xPos = 0;
+let yPos = 0;
+const directions = [];
+let moveRight;
+let moveLeft;
+let moveUp;
+let moveDown;
 
-        
-        context.rect(xPos, yPos, 50, 50);
-        context.stroke();
 
-        function recordMove(e){
+context.rect(xPos, yPos, 50, 50);
+context.stroke();
 
-                //Move Right
-            if(e.keyCode==39){
-               
-                let commandRight = {pos: 'x', operator: '+', value: 20}
-                directions.push(commandRight)
-                console.log(directions)
-            }
-            //Move Left
-            if(e.keyCode==37){
-                let commandLeft = {pos: 'x', operator: '-', value: 20}
-                directions.push(commandLeft)
-                console.log(directions)
-        }   
-            // Move Up
-            if(e.keyCode==38){
-                let commandUp = {pos: 'y', operator: '-', value: 20}
-                directions.push(commandUp)
-                console.log(directions)
-            }
-            //Move Down
-            if(e.keyCode==40){
-                let commandDown = {pos: 'y', operator: '+', value: 20}
-                directions.push(commandDown)
-                console.log(directions)
-        }
-            if(e.keyCode==13)
-            {
-                for (let i = 0; i < directions.length; i++)
-                    { 
-                        if(directions[i].pos === 'x'){
-                            if(directions[i].operator === '+'){
-                                moveRight = xPos+=20
-                                context.rect(xPos, yPos, 50, 50);
-                                context.stroke();
-                            }
-                            if(directions[i].operator === '-'){
-                                moveLeft = xPos-=20
-                                context.rect(xPos, yPos, 50, 50);
-                                context.stroke();
-                            }
-                        }                       
-                        if(directions[i].pos === 'y'){
-                                if(directions[i].operator === '+'){
-                                    moveUp = yPos+=20
-                                    context.rect(xPos, yPos, 50, 50);
-                                     context.stroke();
-                                }
-                                if(directions[i].operator === '-'){
-                                    moveDown = yPos-=20
-                                    context.rect(xPos, yPos, 50, 50);
-                                    context.stroke();
+function recordMove(e){
 
-                            }
-
-                        }
-                        context.rect(xPos, yPos, 50, 50);
-                        context.stroke();
-                    }
-                    
-                        
-                    
-                    // directions.splice(0, directions.length)
-                    directions.length = 0
-            }
-        
-        canvas.width=canvas.width;
-        context.rect(xPos, yPos, 50, 50);
-        context.stroke();
-        
+        //Move Right
+    if(e.keyCode==39){
+       
+        let commandRight = {pos: 'x', operator: '+', value: 50}
+        directions.push(commandRight)
+        console.log(directions)
     }
+    //Move Left
+    if(e.keyCode==37){
+        let commandLeft = {pos: 'x', operator: '-', value: 50}
+        directions.push(commandLeft)
+        console.log(directions)
+}   
+    // Move Up
+    if(e.keyCode==38){
+        let commandUp = {pos: 'y', operator: '-', value: 50}
+        directions.push(commandUp)
+        console.log(directions)
+    }
+    //Move Down
+    if(e.keyCode==40){
+        let commandDown = {pos: 'y', operator: '+', value: 50}
+        directions.push(commandDown)
+        console.log(directions)
+}
+    if(e.keyCode==13)
+    {
+        directions.forEach((direction, i) => {
+            setTimeout(() => {
+                
+                { 
+                    if(direction.pos === 'x'){
+                        if(direction.operator === '+'){
+                            moveRight = xPos+=50
+                        }
+                        if(direction.operator === '-'){
+                            moveLeft = xPos-=50
+                        }
+                    }                       
+                    if(direction.pos === 'y'){
+                            if(direction.operator === '+'){
+                                moveUp = yPos+=50
+                            }
+                            if(direction.operator === '-'){
+                                moveDown = yPos-=50
     
-    document.onkeydown = recordMove;
+                        }
+    
+                    }
+                    context.rect(xPos, yPos, 50, 50);
+                    context.stroke();
+                }
+            },i * 500)
+        })
+            
+                
+            
+            // directions.splice(0, directions.length)
+            directions.length = 0
+    }
+
+canvas.width=canvas.width;
+context.rect(xPos, yPos, 50, 50);
+context.stroke();
+
+}
+
+document.onkeydown = recordMove;
